b8682f8aad5e22fa9e2e6a1df8cd539d9bbcb7dd
Allen Palacios <apalacios1994@gmail.com>
2021-02-17 02:12:22 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -64,7 +64,7 @@ def add_characters
   epic_tragedy[:add_characters][0] = [:name]
   epic_tragedy[:add_characters][1] = [:name]
   
-  epic_tragedy[:add_characters][0][:name] = "Prince Escalus"
+  epic_tragedy[:add_characters][0][:name] = ["Prince Escalus"]
   epic_tragedy[:add_characters][1][:name] = "Apothecary"
 
 
